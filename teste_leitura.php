d4f4f17a03263d8f957065760e65c0922319a517
Nelson Costa <nelsonfilipecosta@gmail.com>
2023-12-27 11:27:11 +0000

    Update teste_leitura.php

--- teste_leitura.php
+++ teste_leitura.php
@@ -1,182 +1,31 @@
-adivinha qual é o meu nome
+<?php
 
+// Configurações do banco de dados SQL
+$host_sql = 'seu_host_sql';
+$dbname_sql = 'seu_nome_de_banco_sql';
+$username_sql = 'seu_usuario_sql';
+$password_sql = 'sua_senha_sql';
 
-772e782f7594b1022937d5148326caca34b862b7
-Node Id
-node_id
-C_kwDOK97lOtoAKDc3MmU3ODJmNzU5NGIxMDIyOTM3ZDUxNDgzMjZjYWNhMzRiODYyYjc
-commit
-commit
-author
-author
-Author Name
-name
-Nelson Costa
-Author Email
-email
-nelsonfilipecosta@gmail.com
-Commit Author Date
-date
-2023-12-26T19:22:56Z
-committer
-committer
-Committer Name
-name
-Nelson Costa
-Committer Email
-email
-nelsonfilipecosta@gmail.com
-Commit Committer Date
-date
-2023-12-26T19:22:56Z
-Commit Message
-message
-teste
+// Configurações do banco de dados MongoDB
+$host_mongo = 'localhost';
+$port_mongo = 27017;
+$dbname_mongo = 'seu_nome_de_banco_mongo';
 
-teste
-tree
-tree
-Commit Tree Sha
-sha
-d615e3d043f0950d105b235628e0c65792d2c0f7
-Commit Tree Url
-url
-https://api.github.com/repos/nelsondcosta23/ai-api/git/trees/d615e3d043f0950d105b235628e0c65792d2c0f7
-Commit Url
-url
-https://api.github.com/repos/nelsondcosta23/ai-api/git/commits/772e782f7594b1022937d5148326caca34b862b7
-Commit Comment Count
-comment_count
-0
-verification
-verification
-Commit Verification Verified
-verified
-false
-Commit Verification Reason
-reason
-unsigned
-Commit Verification Signature
-signature
-Commit Verification Payload
-payload
-Url
-url
-https://github.com/nelsondcosta23/ai-api/tree/772e782f7594b1022937d5148326caca34b862b7
-Html Url
-html_url
-https://github.com/nelsondcosta23/ai-api/commit/772e782f7594b1022937d5148326caca34b862b7
-Comments Url
-comments_url
-https://api.github.com/repos/nelsondcosta23/ai-api/commits/772e782f7594b1022937d5148326caca34b862b7/comments
-author
-author
-Author Login
-login
-nelsondcosta23
-Author Id
-id
-11686595
-Author Node Id
-node_id
-MDQ6VXNlcjExNjg2NTk1
-Author Avatar Url
-avatar_url
-https://avatars.githubusercontent.com/u/11686595?v=4
-Author Gravatar Id
-gravatar_id
-Author Avatar Url
-url
-https://api.github.com/users/nelsondcosta23
-Author Html Url
-html_url
-https://github.com/nelsondcosta23
-Author Followers Url
-followers_url
-https://api.github.com/users/nelsondcosta23/followers
-Author Following Url
-following_url
-https://api.github.com/users/nelsondcosta23/following{/other_user}
-Author Gists Url
-gists_url
-https://api.github.com/users/nelsondcosta23/gists{/gist_id}
-Author Starred Url
-starred_url
-https://api.github.com/users/nelsondcosta23/starred{/owner}{/repo}
-Author Subscriptions Url
-subscriptions_url
-https://api.github.com/users/nelsondcosta23/subscriptions
-Author Organizations Url
-organizations_url
-https://api.github.com/users/nelsondcosta23/orgs
-Author Repos Url
-repos_url
-https://api.github.com/users/nelsondcosta23/repos
-Author Events Url
-events_url
-https://api.github.com/users/nelsondcosta23/events{/privacy}
-Author Received Events Url
-received_events_url
-https://api.github.com/users/nelsondcosta23/received_events
-Author Type
-type
-User
-Author Site Admin
-site_admin
-false
-committer
-committer
-Committer Login
-login
-nelsondcosta23
-Committer Id
-id
-11686595
-Committer Node Id
-node_id
-MDQ6VXNlcjExNjg2NTk1
-Committer Avatar Url
-avatar_url
-https://avatars.githubusercontent.com/u/11686595?v=4
-Committer Gravatar Id
-gravatar_id
-Committer Avatar Url
-url
-https://api.github.com/users/nelsondcosta23
-Committer Html Url
-html_url
-https://github.com/nelsondcosta23
-Committer Followers Url
-followers_url
-https://api.github.com/users/nelsondcosta23/followers
-Committer Following Url
-following_url
-https://api.github.com/users/nelsondcosta23/following{/other_user}
-Committer Gists Url
-gists_url
-https://api.github.com/users/nelsondcosta23/gists{/gist_id}
-Committer Starred Url
-starred_url
-https://api.github.com/users/nelsondcosta23/starred{/owner}{/repo}
-Committer Subscriptions Url
-subscriptions_url
-https://api.github.com/users/nelsondcosta23/subscriptions
-Committer Organizations Url
-organizations_url
-https://api.github.com/users/nelsondcosta23/orgs
-Committer Repos Url
-repos_url
-https://api.github.com/users/nelsondcosta23/repos
-Committer Events Url
-events_url
-https://api.github.com/users/nelsondcosta23/events{/privacy}
-Committer Received Events Url
-received_events_url
-https://api.github.com/users/nelsondcosta23/received_events
-Committer Type
-type
-User
-Committer Site Admin
-site_admin
-false
-Parents
+try {
+    // Conectar ao banco de dados SQL usando PDO
+    $pdo = new PDO("mysql:host=$host_sql;dbname=$dbname_sql", $username_sql, $password_sql);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conectado ao banco de dados SQL\n";
+
+    // Conectar ao banco de dados MongoDB
+    $mongoClient = new MongoDB\Driver\Manager("mongodb://$host_mongo:$port_mongo");
+    $mongoDB = $mongoClient->selectDatabase($dbname_mongo);
+
+    echo "Conectado ao banco de dados MongoDB\n";
+} catch (PDOException $e) {
+    echo "Erro ao conectar ao banco de dados SQL: " . $e->getMessage() . "\n";
+} catch (MongoDB\Driver\Exception\Exception $e) {
+    echo "Erro ao conectar ao banco de dados MongoDB: " . $e->getMessage() . "\n";
+}
+?>
